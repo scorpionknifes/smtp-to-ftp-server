462079bb63e3c025a3af64b4e8c282ee8e3debb7
ZHENK <chengzhenyang@gmail.com>
2021-02-08 04:03:59 +1300

    add support for octet-stream

--- parser.go
+++ parser.go
@@ -18,6 +18,7 @@ const contentTypeMultipartAlternative = "multipart/alternative"
 const contentTypeMultipartRelated = "multipart/related"
 const contentTypeTextHtml = "text/html"
 const contentTypeTextPlain = "text/plain"
+const contentTypeOctetStream = "application/octet-stream"
 
 // Parse an email message read from io.Reader into parsemail.Email struct
 func Parse(r io.Reader) (email Email, err error) {
@@ -72,6 +73,8 @@ func Parse(r io.Reader) (email Email, err error) {
 		}
 
 		email.HTMLBody = strings.TrimSuffix(string(message[:]), "\n")
+	case contentTypeOctetStream:
+		email.Attachments, err = parseAttachmentOnlyEmail(msg.Body, msg.Header)
 	default:
 		email.Content, err = decodeContent(msg.Body, msg.Header.Get("Content-Transfer-Encoding"))
 	}
@@ -123,6 +126,31 @@ func parseContentType(contentTypeHeader string) (contentType string, params map[
 	}
 
 	return mime.ParseMediaType(contentTypeHeader)
+}
+
+func parseAttachmentOnlyEmail(body io.Reader, header mail.Header) (attachments []Attachment, err error) {
+	contentDisposition := header.Get("Content-Disposition")
+
+	if len(contentDisposition) > 0 && strings.Contains(contentDisposition, "attachment;") {
+
+		attachmentData, err := decodeContent(body, header.Get("Content-Transfer-Encoding"))
+		if err != nil {
+			return attachments, err
+		}
+
+		fileName := strings.Replace(contentDisposition, "attachment; filename=\"", "", -1)
+		fileName = strings.TrimRight(fileName, "\"")
+
+		at := Attachment{
+			Filename:    fileName,
+			ContentType: "application/octet-stream",
+			Data:        attachmentData,
+		}
+
+		attachments = append(attachments, at)
+	}
+
+	return attachments, nil
 }
 
 func parseMultipartRelated(msg io.Reader, boundary string) (textBody, htmlBody string, embeddedFiles []EmbeddedFile, err error) {
